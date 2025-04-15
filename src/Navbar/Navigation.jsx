c4fdcbbf0d001595789dff2ba9b9f2ee51dc3da8
Ajay-Upadhyaya <ajay.upd10@gmail.com>
2025-04-15 10:05:24 +0545

    navbar css isactive

--- src/Navbar/Navigation.jsx
+++ src/Navbar/Navigation.jsx
@@ -46,7 +46,7 @@ function Navigation() {
               to="/about"
               className={({ isActive }) =>
                 `md:text-lg text-sm font-semibold hover:text-[#D95103] ${
-                  isActive ? "text-[#0C6967] underline" : "text-[#6B788E]"
+                  isActive ? "text-[#0C6967] text-xl" : "text-[#6B788E] text-lg"
                 }`
               }
             >
@@ -56,7 +56,7 @@ function Navigation() {
               to="/menu"
               className={({ isActive }) =>
                 `md:text-lg text-sm font-semibold hover:text-[#D95103] ${
-                  isActive ? "text-[#0C6967] underline" : "text-[#6B788E]"
+                  isActive ? "text-[#0C6967] text-xl" : "text-[#6B788E] text-lg"
                 }`
               }
             >
@@ -66,7 +66,7 @@ function Navigation() {
               to="/services"
               className={({ isActive }) =>
                 `md:text-lg text-sm font-semibold hover:text-[#D95103] ${
-                  isActive ? "text-[#0C6967] underline" : "text-[#6B788E]"
+                  isActive ? "text-[#0C6967] text-xl" : "text-[#6B788E] text-lg"
                 }`
               }
             >
@@ -77,7 +77,7 @@ function Navigation() {
               to="/allergyadvice"
               className={({ isActive }) =>
                 `md:text-lg text-sm font-semibold hover:text-[#D95103] ${
-                  isActive ? "text-[#0C6967] underline" : "text-[#6B788E]"
+                  isActive ? "text-[#0C6967] text-xl" : "text-[#6B788E] text-lg"
                 }`
               }
             >
